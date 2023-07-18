13aa734d4d60fcc457fb3189846c916d9f505ca3
German1127 <kleverk2023@gmail.com>
2023-07-18 12:09:55 -0700

    main.h with all the necessary prototypes and libraries

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H//crea un bloque de inclusión condicional//
+#define MAIN_H//crea un bloque de inclusión condicional//
 
 //librerias//
 
@@ -7,10 +7,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 /**
- *
+ * struct op - the struct
+ * @op: the operator
+ * @p: the function
  */
-
 //esta funcion devuelve salida estandar//
 typedef struct op
 {
@@ -19,12 +21,16 @@ typedef struct op
 			
 } op_t;
 
-//prototipo//
+//prototipos, parametros y definicion de funciones//
+int _printf(const char *format, ...); //prototipo de la tarea//
+int (*get_op_func(char))(va_list); //define get_op_func com. func. tipo char//
+int printChar(va_list);//fucion que toma parametro tipo list y devuelve int//
+int printString(va_list);//toma un param tipo list y devuelve un entero int//
+int printNum(va_list);//f. que toma un p. de tipo list y devuelve un int//
+int printSign(va_list);//f. que toma un p. de tipo list y devuelve un int//
+int _putchar(char c);//f. que toma un p. de tipo char y devuelve un int//
+int putstring(char *str);//f. que toma un p. de tipo char y devuelve un int//
+int putNum(unsigned long int n, int base, char *digits);
+int printBI(va_list arg);//f. que toma un p. de tipo list y devuelve un int//
 
-int _printf(const char *format, ...);
-
-//prot _putchar//
-
-int _putchar(char c);
-
-#endif
+#endif//#endif se coloca al final del bloque de inclusión condicional//
