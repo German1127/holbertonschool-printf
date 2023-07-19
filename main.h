429db8d31af62532bc6b2ffd775e18f3869faf39
German1127 <kleverk2023@gmail.com>
2023-07-18 21:03:33 -0700

    main.h

--- main.h
+++ main.h
@@ -4,33 +4,28 @@
 //librerias//
 
 #include <stdarg.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 /**
  * struct op - the struct
  * @op: the operator
  * @p: the function
  */
-//esta funcion devuelve salida estandar//
+//Este fragmento de código define una estructura llamada op//
+//utilizando la palabra clave typedef, y luego define un nuevo tipo de dato//
+//llamado op_t que representa a esa estructura//
 typedef struct op
 {
 	char op;
 	int (*f)(va_list);
-			
 } op_t;
 
 //prototipos, parametros y definicion de funciones//
 int _printf(const char *format, ...); //prototipo de la tarea//
-int (*get_op_func(char))(va_list); //define get_op_func com. func. tipo char//
-int printChar(va_list);//fucion que toma parametro tipo list y devuelve int//
-int printString(va_list);//toma un param tipo list y devuelve un entero int//
-int printNum(va_list);//f. que toma un p. de tipo list y devuelve un int//
-int printSign(va_list);//f. que toma un p. de tipo list y devuelve un int//
-int _putchar(char c);//f. que toma un p. de tipo char y devuelve un int//
-int putstring(char *str);//f. que toma un p. de tipo char y devuelve un int//
-int putNum(unsigned long int n, int base, char *digits);
-int printBI(va_list arg);//f. que toma un p. de tipo list y devuelve un int//
-
+int get_function(char s, va_list args);
+int _putchar(char c);
+int printchar(va_list args);
+int printstring(va_list args);
+int printdigit(va_list args);
+int printsign(va_list args);
 #endif//#endif se coloca al final del bloque de inclusión condicional//
