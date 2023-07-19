7c4933df7b0d65ff86c9ce1c4b7fb9e9a4a047a7
German1127 <kleverk2023@gmail.com>
2023-07-19 06:30:01 -0700

    Functions implement printing of different types of formats for the _printf function

--- get_function.c
+++ get_function.c
@@ -6,7 +6,11 @@
  * @args: arguments.
  * Return: char count.
  */
-
+/*La función get_function busca y ejecuta la función de impresión 
+ correspondiente al tipo de formato especificado por t_step. Devuelve el 
+ número de caracteres impresos por la función de formato o 0 si no se 
+ encuentra una función correspondiente. Se utiliza en conjunto con la función
+ printf para procesar formatos específicos*/
 int get_function(char t_step, va_list args)
 {
 	int i = 0;
@@ -20,7 +24,12 @@ int get_function(char t_step, va_list args)
 		{'i', printdigit},
 		{0, NULL}
 	};
-
+/*El código busca en la estructura step una función de impresión 
+ correspondiente al tipo de formato t_step. Si encuentra una coincidencia
+  llama a esa función y suma el resultado al contador count
+  Si no encuentra una función correspondiente, imprime los caracteres '%'
+  y t_step y también los cuenta. Al final, devuelve el número total de 
+  caracteres impresos*/
 	while (step[i].op)
 	{
 		if (t_step == step[i].op)
@@ -64,13 +73,20 @@ int printchar(va_list args)
  * Return: count chars.
  */
 
+/*La función printdigit procesa un dígito recibido como argumento variable 
+ y lo imprime como parte del formato de impresión. Devuelve el número 
+ de caracteres impresos. Se utiliza en conjunto con la función 
+ _printf para imprimir dígitos específicos*/
 int printdigit(va_list args)
 {
 	int decimal = 1;
 	int count = 0;
 	long int digit = va_arg(args, int);
 	long int digit2;
-
+/*La función printdigit imprime un dígito como parte del formato de impresión. 
+ Si el dígito es negativo, se imprime un signo '-' y se convierte a 
+ positivo. Luego, se itera a través del dígito y se imprimen los dígitos 
+ individualmente. Al final, se devuelve el número de caracteres impresos*/
 	if (digit < 0)
 	{
 		count += _putchar('-');
@@ -104,7 +120,9 @@ int printdigit(va_list args)
  * @args: arguments.
  * Return: count chars.
  */
-
+/*La función printsign imprime el carácter '%' y devuelve 1. Se utiliza en 
+ conjunto con la función _printf para imprimir el signo '%' 
+ literal en el formato de impresión*/
 int printsign(va_list args)
 {
 	(void)args;
@@ -123,7 +141,11 @@ int printsign(va_list args)
  * @args: argument.
  * Return: character count.
  */
-
+/*La función printstring imprime una cadena de caracteres recibida como a
+ rgumento variable y devuelve el número de caracteres impresos. 
+ Si la cadena es nula, se imprime la cadena "(null)". Si la cadena está 
+ vacía, devuelve -1. Se utiliza en conjunto con la función _printf para 
+ imprimir cadenas de caracteres específicas*/
 int printstring(va_list args)
 {
 	int i;
