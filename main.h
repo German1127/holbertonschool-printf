dd81e9c2995168ba13d926bfcdcede11d0b32413
German1127 <kleverk2023@gmail.com>
2023-07-18 21:50:06 -0700

    main.h

--- main.h
+++ main.h
@@ -1,19 +1,15 @@
-#ifndef MAIN_H//crea un bloque de inclusión condicional//
-#define MAIN_H//crea un bloque de inclusión condicional//
-
-//librerias//
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * struct op - the struct
- * @op: the operator
- * @p: the function
+ * struct format - struct
+ * @specifiers: struct format
+ * @f: the function
  */
-//Este fragmento de código define una estructura llamada op//
-//utilizando la palabra clave typedef, y luego define un nuevo tipo de dato//
-//llamado op_t que representa a esa estructura//
+
 typedef struct op
 {
 	char op;
@@ -24,8 +20,10 @@ typedef struct op
 int _printf(const char *format, ...); //prototipo de la tarea//
 int get_function(char s, va_list args);
 int _putchar(char c);
+
 int printchar(va_list args);
 int printstring(va_list args);
 int printdigit(va_list args);
 int printsign(va_list args);
-#endif//#endif se coloca al final del bloque de inclusión condicional//
+
+#endif
