a430d0e7737b9ef75c2805385b1d6ee2ae374b3e
Nitsu47 <135637506+Nitsu47@users.noreply.github.com>
2023-07-27 11:02:17 -0300

    comments fixed in get_function.c

--- get_function.c
+++ get_function.c
@@ -6,11 +6,14 @@
  * @args: arguments.
  * Return: char count.
  */
-/*La función get_function busca y ejecuta la función de impresión 
- correspondiente al tipo de formato especificado por t_step. Devuelve el 
- número de caracteres impresos por la función de formato o 0 si no se 
- encuentra una función correspondiente. Se utiliza en conjunto con la función
- printf para procesar formatos específicos*/
+
+/*
+The get_function function finds and executes the print function
+corresponding to the format type specified by t_step. Return the
+number of characters printed by the format function or 0 if not
+find a corresponding function. It is used in conjunction with the function
+printf to process specific formats
+*/
 int get_function(char t_step, va_list args)
 {
 	int i = 0;
@@ -24,12 +27,14 @@ int get_function(char t_step, va_list args)
 		{'i', printdigit},
 		{0, NULL}
 	};
-/*El código busca en la estructura step una función de impresión 
- correspondiente al tipo de formato t_step. Si encuentra una coincidencia
-  llama a esa función y suma el resultado al contador count
-  Si no encuentra una función correspondiente, imprime los caracteres '%'
-  y t_step y también los cuenta. Al final, devuelve el número total de 
-  caracteres impresos*/
+/*
+The code looks in the step structure for a print function
+corresponding to the format type t_step. If you find a match
+call that function and add the result to the counter count
+If it doesn't find a corresponding function, it prints the characters '%'
+and t_step and also counts them. At the end, it returns the total number of
+printed characters
+*/
 	while (step[i].op)
 	{
 		if (t_step == step[i].op)
@@ -73,20 +78,24 @@ int printchar(va_list args)
  * Return: count chars.
  */
 
-/*La función printdigit procesa un dígito recibido como argumento variable 
- y lo imprime como parte del formato de impresión. Devuelve el número 
- de caracteres impresos. Se utiliza en conjunto con la función 
- _printf para imprimir dígitos específicos*/
+/*
+The printdigit function processes a digit received as a variable argument.
+and prints it as part of the print format. returns the number
+of printed characters. It is used in conjunction with the function
+_printf to print specific digits
+*/
 int printdigit(va_list args)
 {
 	int decimal = 1;
 	int count = 0;
 	long int digit = va_arg(args, int);
 	long int digit2;
-/*La función printdigit imprime un dígito como parte del formato de impresión. 
- Si el dígito es negativo, se imprime un signo '-' y se convierte a 
- positivo. Luego, se itera a través del dígito y se imprimen los dígitos 
- individualmente. Al final, se devuelve el número de caracteres impresos*/
+/*
+The printdigit function prints a digit as part of the print format.
+If the digit is negative, a '-' sign is printed and converted to
+positive. Then iterates through the digit and prints the digits
+individually. At the end, the number of characters printed is returned
+*/
 	if (digit < 0)
 	{
 		count += _putchar('-');
@@ -120,9 +129,12 @@ int printdigit(va_list args)
  * @args: arguments.
  * Return: count chars.
  */
-/*La función printsign imprime el carácter '%' y devuelve 1. Se utiliza en 
- conjunto con la función _printf para imprimir el signo '%' 
- literal en el formato de impresión*/
+
+/*
+The printsign function prints the character '%' and returns 1. It is used in
+set with function _printf to print the sign '%'
+verbatim in print format
+*/
 int printsign(va_list args)
 {
 	(void)args;
@@ -141,11 +153,13 @@ int printsign(va_list args)
  * @args: argument.
  * Return: character count.
  */
-/*La función printstring imprime una cadena de caracteres recibida como a
- rgumento variable y devuelve el número de caracteres impresos. 
- Si la cadena es nula, se imprime la cadena "(null)". Si la cadena está 
- vacía, devuelve -1. Se utiliza en conjunto con la función _printf para 
- imprimir cadenas de caracteres específicas*/
+/*
+The printstring function prints a received character string as a
+variable argument and returns the number of characters printed.
+If the string is null, the string "(null)" is printed. If the chain is
+empty, returns -1. It is used in conjunction with the _printf function to
+print specific character strings
+*/
 int printstring(va_list args)
 {
 	int i;
