7e595dc363a2bcdb94e830b424fa23f36d35456f
Nitsu47 <135637506+Nitsu47@users.noreply.github.com>
2023-07-27 09:51:33 -0300

    commetns fixed in _printf.c

--- _printf.c
+++ _printf.c
@@ -6,31 +6,34 @@
  *Return: count chars.
  */
 int _printf(const char *format, ...)
-{/*Se declaran variables locales i, count y count_2 de tipo int
-   i se utiliza como un contador para recorrer la cadena
-   format, count lleva la cuenta total de caracteres impresos y 
-   count_2 se utiliza para llevar la cuenta de
-   caracteres impresos por las funciones de formato.*/
+{/*Local variables i, count and count_2 of type int are declared
+   i is used as a counter to loop through the string
+   format, count keeps the total count of characters printed and
+   count_2 is used to keep track of
+   characters printed by the format functions.
+   */
 	int i = 0;
 	int	count = 0;
 	int count_2;
 	va_list args;
-/*Estas líneas de código verifican si el argumento format cumple con ciertas
- condiciones antes de procesar los argumentos variables. Si format es NULL, 
- si comienza con '%' seguido de un carácter nulo, o si comienza con '%' 
- seguido de un espacio en blanco y el siguiente carácter es nulo, 
- entonces se devuelve -1 para indicar un error*/
+/*These lines of code check if the format argument meets certain
+ conditions before processing the variable arguments. If format is NULL,
+ if it starts with '%' followed by a null character, or if it starts with '%'
+ followed by a white space and the next character is null,
+ then -1 is returned to indicate an error
+ */
 	va_start(args, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-/*Este bucle recorre la cadena format y procesa los caracteres uno por uno
-Si encuentra un carácter '%' seguido de un formato válido
-llama a una función correspondiente y cuenta los caracteres impresos
-i encuentra un carácter normal, lo imprime y también cuenta los caracteres
-Al final, devuelve el número total de caracteres impresos o -1 en caso de 
-error*/
+/*This loop iterates through the format string and processes the characters one by one
+If you encounter a '%' character followed by a valid format
+calls a corresponding function and counts the characters printed
+i find a normal character, print it and also count the characters
+At the end, it returns the total number of characters printed or -1 in case of
+mistake
+*/
 	while (format[i])
 	{
 		count_2 = 0;
@@ -56,8 +59,9 @@ error*/
 		if (count != -1)
 			count += count_2;
 	}
-/*Estas líneas de código finalizan el uso de los argumentos variables y 
- devuelven el número total de caracteres impresos por la función _printf.*/
+/*These lines of code end the use of the variable arguments and
+ return the total number of characters printed by the _printf function.
+ */
 	va_end(args);
 	return (count);
 }
